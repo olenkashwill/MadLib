e0619f933e806bcd50b66dc7dab476e460bbeb65
Olenka Ashwill <olenkashwill@gmail.com>
2018-07-24 15:31:08 -0500

    commiting my mad libs code

---
+++ script2.js
@@ -0,0 +1,29 @@
+ $(document).ready(function() {
+	 
+		
+		
+		
+		$("button").click(function(){
+		var color = $("#color").val(); console.log ("color");
+		var genre = $("#genre").val(); console.log ("genre");
+		var noun = $("#noun").val(); console.log ("noun");
+		var bandname = $("#bandname").val(); 
+		var releasedate = $("#releasedate").val();
+		var albumtitle = $("#albumtitle").val();
+		var format = $("#record label").val();
+		
+		
+		var finalsentence = color + genre + noun, + band name + "have announced a" + release date + 
+		});
+		
+		
+		
+		
+		
+
+
+		
+		
+		
+		
+ });
