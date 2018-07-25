7615606ab1c84ceb8ea556dd7ee0def160985bad
Olenka Ashwill <41594769+olenkashwill@users.noreply.github.com>
2018-07-25 10:57:52 -0500

    Add files via upload

--- script2.js
+++ script2.js
@@ -4,26 +4,20 @@
 		
 		
 		$("button").click(function(){
-		var color = $("#color").val(); console.log ("color");
-		var genre = $("#genre").val(); console.log ("genre");
-		var noun = $("#noun").val(); console.log ("noun");
-		var bandname = $("#bandname").val(); 
-		var releasedate = $("#releasedate").val();
-		var albumtitle = $("#albumtitle").val();
-		var format = $("#record label").val();
+		var color = $("#color").val(); console.log(color);
+		var genre = $("#genre").val(); console.log(genre);
+		var noun = $("#noun").val(); console.log(noun);
+		var bandname = $("#bandname").val(); console.log(bandname);
+		var releasedate = $("#releasedate").val(); console.log(releasedate);
+		var albumtitle = $("#albumtitle").val(); console.log(albumtitle);
+		var recordlabel = $("#recordlabel").val(); console.log(recordlabel);
+		var format = $("#format").val(); console.log(format);
 		
+		var finalsentence = color + " " + genre + " " + noun + ", " + bandname + ", have announced a " + releasedate + " release of their highly anticipated album, " + albumtitle + ", which will come out via " + recordlabel + " on " + format;
 		
-		var finalsentence = color + genre + noun, + band name + "have announced a" + release date + 
-		});
-		
-		
-		
-		
-		
-
-
+		$("#result").html(finalsentence);
+				});
 		
 		
 		
-		
- });
+		 });
